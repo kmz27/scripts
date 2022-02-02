d2f8f6fd814c935c562fae16af42bb93668dbcd3
kmz27 <44536631+kmz27@users.noreply.github.com>
2022-02-02 13:47:16 +0100

    Improved logging

--- ps/get-remoteusers.ps1
+++ ps/get-remoteusers.ps1
@@ -1,6 +1,19 @@
 # Written by https://github.com/kmz27
-$output = {
+$output1 = {
+"------"
 "Log created on:"
 get-date -f "dd-MM-yyyy HH:mm"
-get-localgroupmember remotedesktopbenutzer | select Name}
-& $output | Out-File -append \\{LOGPATH}\$env:computername.log -NoClobber
+get-localgroupmember remotedesktopbenutzer | select Name
+}
+& $output1 | Out-File -append \\{LOGPATH}\$env:computername.log -NoClobber
+
+
+$output2 = {
+    $Main = get-localgroupmember remotedesktopbenutzer | select Name, PrincipalSource
+    foreach ($Users in $Main) {
+        if ($Users.PrincipalSource -eq "ActiveDirectory") {
+        "$(get-date -f 'dd-MM-yyyy HH:mm') $env:computername hat $(get-localgroupmember remotedesktopbenutzer | select Name)"
+        }
+    }
+}
+& $output2 | Out-File -append \\{LOGPATH}\all.log -NoClobber
